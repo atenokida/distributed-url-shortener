a2394cfeb9bd54a6a69b8215e65bc3a0ad3527bf
atenokida <alexandretolomeotti@alunos.utfpr.edu.br>
2025-07-04 17:51:56 -0300

    Add URL size config

--- src/config/index.js
+++ src/config/index.js
@@ -9,15 +9,21 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+// Environment variables for the application
 export const server = { 
   port: process.env.PORT || 3000,
   baseUrl: process.env.BASE_URL || `http://localhost:${process.env.PORT || 3000}`
 };
 
+// Environment variables for Redis
 export const redis = { url: process.env.REDIS_URL };
 
+// Environment variables for Cassandra
 export const cassandra = {
   contactPoints: process.env.CASSANDRA_CONTACT_POINTS.split(','),
   localDataCenter: process.env.CASSANDRA_LOCAL_DATACENTER,
   keyspace: process.env.CASSANDRA_KEYSPACE
 };
+
+// Size of the short URL code
+export const URL_SIZE = 9;
